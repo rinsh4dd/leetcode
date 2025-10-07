2898ad711e10f7ff9ce97dacb850736b148bc9ae
rinsh4dd <rinshadcontacts@gmail.com>
2025-10-07 11:49:09 +0530

    2894

---
+++ LeetCodeC#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        int n = 10;
+        int m = 3;
+
+        int result = solution.DifferenceOfSums(n, m);
+        Console.WriteLine($"Difference of sums for n={n}, m={m} is: {result}");
+    }
+}
+
+public class Solution
+{
+    public int DifferenceOfSums(int n, int m)
+    {
+        int div = 0;
+        int nonDiv = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % m == 0)
+                div += i;
+            else
+                nonDiv += i;
+        }
+
+        return nonDiv - div;
+    }
+}
